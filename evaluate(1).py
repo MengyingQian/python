a941da55cd13243acb458354fcb4222edd97d2d1
MengyingQian <656206843@qq.com>
2018-12-10 15:42:49 +0800

    训练集与测试集调参结束

--- evaluate(1).py
+++ evaluate(1).py
@@ -13,11 +13,12 @@ import sys
 import os.path
 #import time
 import scipy.io as scio
-
+import matplotlib.pyplot as plt
 import inference
 
 FLAGS = None
-
+lossList = []
+trainsteps = []
 
 def main(_):
     Samples_placeholder = tf.placeholder(dtype=tf.float32,shape=[None,89],name='X_input')
@@ -34,42 +35,52 @@ def main(_):
     evaluation = inference.evaluation(logits, Labels_placeholder)
     
     saver = tf.train.Saver()
+    nowstep=999
+
+    for i in range(100):
+        model_dir = f'./logfile/train.ckpt-{nowstep:d}'  # tf.train.latest_checkpoint('./logfile')
+
+        with tf.Session() as sess:
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess,coord=coord)
 
 
-    
-    with tf.Session() as sess:
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess,coord=coord) 
-        
 
-        model_dir = './model/train.ckpt-99999'#tf.train.latest_checkpoint('./logfile')
 
-        saver.restore(sess, model_dir)
-        
-        test_data_file = os.path.join(FLAGS.data_dir,'Data_o.mat' )
-        if not tf.gfile.Exists(test_data_file):
-            raise ValueError('Failed to find file: ' + test_data_file)
-        
-        
-        data = scio.loadmat(test_data_file)
-        samples = np.array(data['Train_in'],dtype=np.float32)
-        labels = np.array(data['Train_out'], dtype=np.float32)
-        
-        try:
+            saver.restore(sess, model_dir)
 
-            eval_value,predict = sess.run([evaluation,logits], feed_dict={Samples_placeholder:samples,Labels_placeholder:labels})
-            print("Loss of testing NN" )
-            print(eval_value)
-            test_file = os.path.join(FLAGS.data_dir,'test_result.mat' )
-            predict=predict.astype(np.int32)
-            scio.savemat(test_file,{'Predict':predict})
-                   
-        except tf.errors.OutOfRangeError:
-            print('Done testing --epoch limit reached')
-            
-        finally:
-            coord.request_stop()
-            coord.join(threads)
+            test_data_file = os.path.join(FLAGS.data_dir,'Data_o.mat' )
+            if not tf.gfile.Exists(test_data_file):
+                raise ValueError('Failed to find file: ' + test_data_file)
+
+
+            data = scio.loadmat(test_data_file)
+            samples = np.array(data['Train_in'],dtype=np.float32)
+            labels = np.array(data['Train_out'], dtype=np.float32)
+
+            try:
+
+                eval_value,predict = sess.run([evaluation,logits], feed_dict={Samples_placeholder:samples,Labels_placeholder:labels})
+                loss_value, _ = sess.run([loss, train_op],feed_dict={Samples_placeholder: samples, Labels_placeholder: labels})
+                print("Loss of testing NN" )
+                print(eval_value)
+                print(loss_value)
+                lossList.append(loss_value)
+                test_file = os.path.join(FLAGS.data_dir,'test_result.mat' )
+                predict=predict.astype(np.int32)
+                scio.savemat(test_file,{'Predict':predict})
+
+            except tf.errors.OutOfRangeError:
+                print('Done testing --epoch limit reached')
+
+            finally:
+                coord.request_stop()
+                coord.join(threads)
+        trainsteps.append(nowstep + 1)
+        nowstep=nowstep+1000
+        print(nowstep)
+    plt.plot(trainsteps, lossList, label='First Line')
+    plt.show()
     
 if __name__ =='__main__':
     parser = argparse.ArgumentParser()
--- prediction.py
+++ prediction.py
@@ -25,7 +25,7 @@ import scipy.io as scio
 FLAGS = None
 lossList = []
 trainsteps = []
-
+testlossList = []
 
 def main(_):
     Samples_placeholder = tf.placeholder(dtype=tf.float32,shape=[None,89],name='X_input')
@@ -41,7 +41,7 @@ def main(_):
     
     evaluation = inference.evaluation(logits, Labels_placeholder)
   
-    saver = tf.train.Saver()
+    saver = tf.train.Saver(max_to_keep=0)
     summary = tf.summary.merge_all()    
     init = tf.global_variables_initializer()
     
@@ -63,7 +63,8 @@ def main(_):
         
         samples = np.array(data['Train_data'],dtype=np.float32)
         labels = np.array(data['Train_label'],dtype=np.float32)
-        
+        testsamples = np.array(data['Train_in'], dtype=np.float32)
+        testlabels = np.array(data['Train_out'], dtype=np.float32)
         train_samples = samples
         train_labels = labels
         test_samples = samples
@@ -76,28 +77,31 @@ def main(_):
             start_time = time.time()            
             loss_value,_=sess.run([loss,train_op],feed_dict={Samples_placeholder:samples,Labels_placeholder:labels})
             duration=time.time()-start_time
-            if (i+1) % 1000 == 0:
+            if (i+1) % 100 == 0:
                 lossList.append(loss_value)
                 trainsteps.append(i+1)
-                print("Loss of training NN, step: %d, loss: %f, and time:%f" % (i+1,loss_value,duration))
-            #if (i+1) % 100 == 0 :
-            #    eval_value = sess.run(evaluation, feed_dict={Samples_placeholder:train_samples,Labels_placeholder:train_labels})
-                
+                #print("Loss of training NN, step: %d, loss: %f, and time:%f" % (i+1,loss_value,duration))
+                checkpoint_path = os.path.join(FLAGS.train_dir, 'train.ckpt')
+                saver.save(sess, checkpoint_path, global_step=i)
+            if (i+1) % 100 == 0 :
+                #eval_value = sess.run(evaluation, feed_dict={Samples_placeholder:train_samples,Labels_placeholder:train_labels})
+
                 #print("Evaluation of test samples, step: %d, loss: %f." % (i+1,eval_value))
                 #summary_writer.add_summary(summary_str, i)
-
-                
+                testloss_value, _ = sess.run([loss, train_op],feed_dict={Samples_placeholder: testsamples, Labels_placeholder: testlabels})
+                testlossList.append(testloss_value)
             if i%1000 == 0 or (i+1) == FLAGS.max_steps:
                 eval_value = sess.run(evaluation, feed_dict={Samples_placeholder:samples,Labels_placeholder:labels})
                 print(eval_value)
                 
                 
-        checkpoint_path = os.path.join(FLAGS.train_dir, 'train.ckpt')
-        saver.save(sess, checkpoint_path, global_step=i)        
+       #checkpoint_path = os.path.join(FLAGS.train_dir, 'train.ckpt')
+       # saver.save(sess, checkpoint_path, global_step=i)
         predict = sess.run(logits, feed_dict={Samples_placeholder:test_samples,Labels_placeholder:test_labels})        
         test_file = os.path.join(FLAGS.data_dir,'predict.mat' )
         scio.savemat(test_file,{'Predict':predict})
-        plt.plot(trainsteps, lossList, label='First Line')
+        plt.plot(trainsteps, lossList, label='First Line', color="#F08080")
+        plt.plot(trainsteps, testlossList, label='Second Line',color="#DB7093")
         plt.show()
 
     
